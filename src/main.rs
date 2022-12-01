2a1082228535ec7aaf68e79f1b4ce3f52ac3e8e7
Nevyn Bengtsson <nevyn@alloverse.com>
2022-12-01 11:34:31 +0100

    solution for 01

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead};
+
+#[derive(Debug)]
+struct Elf {
+    cals: u32,
+}
+
+fn main() -> Result<(), Box<dyn Error>>
+{
+    let mut elves: Vec<Elf> = Vec::new();
+    let mut current_elf = Elf { cals: 0 };
+
+    let file = File::open("input.txt")?;
+    let lines = io::BufReader::new(file).lines();
+    for line in lines
+    {
+        let line = line?;
+        match line.parse::<u32>() {
+            Ok(cals) => current_elf.cals += cals,
+            Err(_) => {
+                elves.push(current_elf);
+                current_elf = Elf { cals: 0 };
+            },
+        }
+    }
+    elves.push(current_elf);
+
+    elves.sort_by_key(|k| k.cals );
+    let mut top_three = elves.into_iter().rev().take(3);
+
+    let top = top_three.next().ok_or("too few elves")?;
+    println!("Winning elf: {}", top.cals);
+
+    let three_sum :u32 = top_three.map(|elf| elf.cals ).sum();
+    println!("Together: {}", three_sum);
+
+    Ok(())
+}
